cb83628a1f19a37d8de727bea4b184f9a3961809
vkushnarenko-virtru <vkushnarenko@lohika.com>
2018-07-22 19:52:22 +0300

    Chapter 8 C3 finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,4 +20,30 @@ Product.create!(title: 'Seven Mobile Apps in Seven Weeks',
       </p>},
                 image_url: '7apps.png',
                 price: 26.00)
+
+Product.create!(title: 'Happy coding',
+                description:
+                    %{<p>
+      <em>Native Apps, Multiple Platforms</em>
+      Answer the question “Can we build this for ALL the devices?” with a
+      resounding YES. This book will help you get there with a real-world
+      introduction to seven platforms, whether you’re new to mobile or an
+      experienced developer needing to expand your options. Plus, you’ll find
+      out which cross-platform solution makes the most sense for your needs.
+      </p>},
+                image_url: 'ror.jpg',
+                price: 78.00)
+
+Product.create!(title: 'Something to Read',
+                description:
+                    %{<p>
+      <em>Native Apps, Multiple Platforms</em>
+      Answer the question “Can we build this for ALL the devices?” with a
+      resounding YES. This book will help you get there with a real-world
+      introduction to seven platforms, whether you’re new to mobile or an
+      experienced developer needing to expand your options. Plus, you’ll find
+      out which cross-platform solution makes the most sense for your needs.
+      </p>},
+                image_url: 'rails_books.jpg',
+                price: 99.99)
 #...
